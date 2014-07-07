2e3aa34b1f83d952e8e02ba4b3f2b0ffc752e566
Jim Cochrane <jim.cochrane@gmail.com>
2014-07-07 03:46:45 -0500

    Added all rails-generated files for tradable-analysis-toolset
    Checked in all of the files generated by running:
    rails new tradable-analysis-toolset
    I don't know if all of these files need to be under revision control,
    but this seems to be the easiest way to synch everything between the
    home system and codio.com, using git.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradableAnalysisToolset::Application.config.secret_token = '5992d52822c23bc8c50c49e785d6f229701205efa408f23ea2b4780f35b9540c7d2aa0c44d79b4b7bbf1e1cc2d7fb0aa175bd52207774c1e9b1765fd9d0faba7'
