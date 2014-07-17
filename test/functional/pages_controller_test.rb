d91af56a5b10a43566910c72104d046656aeac41
Jim Cochrane <jim.cochrane@gmail.com>
2014-07-17 07:32:07 -0500

    changed title test to use regex

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -4,14 +4,15 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select 'title', "TradableAnalysisToolset | Home"
+    assert_select 'title', /(TradableAnalysisToolset|TAT)\s*\|\s*Home/
     assert response.body.length > 16, "body exists"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select 'title', "TradableAnalysisToolset | About"
+    assert_select 'title',
+      Regexp.new("(TradableAnalysisToolset|TAT)\s*\|\s*About")
     assert response.body.length > 16, "body exists"
   end
 
