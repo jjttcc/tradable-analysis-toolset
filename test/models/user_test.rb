13d8fabb71712ba23fec7d74f31a65ffbdbc9a8e
Jim Cochrane <jim.cochrane@gmail.com>
2014-07-19 01:31:20 -0500

    UserTest - password tests
    Added some password-related validation tests - checking in before slight
    redesign.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,8 +12,17 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
 
+  GOOD_ARGS1 = {:email_addr => 'user1@example.org'}
+  GOOD_ARGS2 = {:email_addr => 'tester@professional-testers.org'}
+  BAD_ARGS1 = {:email_addr => 'tester@professional#testers.org'}
+
   def valid_user
-    @valid_user ||= User.new(:email_addr => 'tester@professional-testers.org')
+    if @valid_user == nil
+      @valid_user = User.new(GOOD_ARGS1)
+      @valid_user.password = 'foobar'
+      @valid_user.save
+    end
+    @valid_user
   end
 
   def invalid_user
@@ -37,6 +46,8 @@ class UserTest < ActiveSupport::TestCase
       User.new(:email_addr => 'Ordinary@BORING.org'),
     ]
   end
+
+  ### Basic validation ###
 
   def test_valid
     assert valid_user.valid?
@@ -66,6 +77,38 @@ class UserTest < ActiveSupport::TestCase
   def test_duplicate_email_with_different_case
     duplicate_emails[0].save
     assert ! duplicate_emails[2].valid?, "non-unique email address"
+  end
+
+  ### Password ###
+
+  def test_has_password_attribute
+    assert valid_user.respond_to?(:password), "responds to 'password'"
+  end
+
+  def test_has_password_conf_attribute
+    assert valid_user.respond_to?(:password_confirmation)
+  end
+
+  ### Password validation ###
+
+  def new_user(e)
+    result = User.new(:email_addr => e)
+    result.save
+    result
+  end
+
+  def test_has_password
+    pwlessu = new_user('foo@foo.org')
+    pwlessu.password = ''
+    pwlessu.password_confirmation = ''
+    assert(! pwlessu.valid?, "invalid without password")
+  end
+
+  def test_valid_pw_conf
+    u = new_user('foo2@foo.org')
+    u.password = 'foo'
+    u.password_confirmation = 'bar'
+    assert(! u.valid?, "invalid without matching password_conf")
   end
 
 end
