04012391927b745896b85fe1f56618f67894f10a
Jim Cochrane <jim.cochrane@gmail.com>
2014-07-19 01:52:29 -0500

    TestUser
    Implemented redesign mentioned in previous commit - changed a couple
    places that did 'u = User.new(...); u.save' to call 'u = User.create!(...)'
    instead; etc.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,15 +12,15 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
 
-  GOOD_ARGS1 = {:email_addr => 'user1@example.org'}
-  GOOD_ARGS2 = {:email_addr => 'tester@professional-testers.org'}
-  BAD_ARGS1 = {:email_addr => 'tester@professional#testers.org'}
+  GOOD_ARGS1 = {:email_addr => 'user1@example.org', :password => 'foobar',
+                :password_confirmation => 'foobar'}
+  GOOD_ARGS2 = {:email_addr => 'tester@professional-testers.org',
+                :password => 'barfoo', :password_confirmation => 'barfoo'}
+  BAD_EMAIL1 = {:email_addr => 'tester@professional#testers.org'}
 
   def valid_user
     if @valid_user == nil
-      @valid_user = User.new(GOOD_ARGS1)
-      @valid_user.password = 'foobar'
-      @valid_user.save
+      @valid_user = User.create!(GOOD_ARGS1)
     end
     @valid_user
   end
@@ -31,19 +31,19 @@ class UserTest < ActiveSupport::TestCase
 
   def bad_email_users
     @bad_email_users ||= [
-      User.new(:email_addr => 'tester@professional#testers.org'),
-      User.new(:email_addr => '*tester@what?'),
-      User.new(:email_addr => '@what.org'),
-      User.new(:email_addr => '@@bomb.org'),
-      User.new(:email_addr => 'la-bomba@la-bomba@.org'),
+      User.new(GOOD_ARGS1.merge(BAD_EMAIL1)),
+      User.new(GOOD_ARGS1.merge(:email_addr => '*tester@what?')),
+      User.new(GOOD_ARGS1.merge(:email_addr => '@what.org')),
+      User.new(GOOD_ARGS1.merge(:email_addr => '@@bomb.org')),
+      User.new(GOOD_ARGS1.merge(:email_addr => 'la-bomba@la-bomba@.org')),
     ]
   end
 
   def duplicate_emails
     @duplicate_emails ||= [
-      User.new(:email_addr => 'ordinary@boring.org'),
-      User.new(:email_addr => 'ordinary@boring.org'),
-      User.new(:email_addr => 'Ordinary@BORING.org'),
+      User.new(GOOD_ARGS1.merge(:email_addr => 'ordinary@boring.org')),
+      User.new(GOOD_ARGS1.merge(:email_addr => 'ordinary@boring.org')),
+      User.new(GOOD_ARGS1.merge(:email_addr => 'Ordinary@BORING.org')),
     ]
   end
 
@@ -65,7 +65,7 @@ class UserTest < ActiveSupport::TestCase
     /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/)
 =end
     bad_email_users.each do |u|
-      assert (not u.valid?)
+      assert(! u.valid?, "invalid email address not detected")
     end
   end
 
@@ -92,12 +92,11 @@ class UserTest < ActiveSupport::TestCase
   ### Password validation ###
 
   def new_user(e)
-    result = User.new(:email_addr => e)
-    result.save
+    result = User.create!(GOOD_ARGS1.merge(:email_addr => e))
     result
   end
 
-  def test_has_password
+  def test_no_password
     pwlessu = new_user('foo@foo.org')
     pwlessu.password = ''
     pwlessu.password_confirmation = ''
