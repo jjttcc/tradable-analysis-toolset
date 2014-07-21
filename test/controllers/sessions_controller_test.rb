2171d7882ef46add071250866fe7c705bc6920eb
Jim Cochrane <jim.cochrane@gmail.com>
2014-07-21 08:28:04 -0500

    Work on login testing with minitest
    SessionsControllerTest: It took some work to get the successful login test
    working, because of the complexity of the login process and because the
    tutorial uses rspec while I'm using minitest.  The main login test -
    test_good_redirect - now succeeds.  Committing this work before cleaning up
    slop left over from this work and deleting 'test_good_values', which fails
    because it calls:
          assert_response :success, 'normal response expected'
    but the response is a redirect rather than the :success (200, I think)
    expected here.  The tutorial does:
    controller.current_user.should == @user
    controller.should be_signed_in
    
    but there's no 'controller' method or attribute in this class (with
    minitest) and I've not figured out how to find and check this state.  So
    I've concluded that test_good_redirect is enough for now and
    test_good_values can go.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -17,6 +17,47 @@ class SessionsControllerTest < ActionController::TestCase
 
   def setup
     @bad_attr = {:email_addr => '', :password => ''}
+    @good_attr = {
+      :email_addr            => 'iexist@test.org',
+      :password              => 'existence-is-futile',
+      :password_confirmation => 'existence-is-futile',
+    }
+    @dbuser = User.find_by_email_addr(@good_attr[:email_addr])
+    if @dbuser == nil
+      # The user is not yet in the database.
+      @dbuser = User.create!(@good_attr)
+      if @dbuser == nil
+        @dbuser = @user
+        throw "Retrieval of user at #{@good_attr[:email_addr]} failed."
+      end
+else
+puts "<<<<<<<<<<<<<#{@dbuser.email_addr} was in the database.>>>>>>>>>>>>>"
+    end
+puts "<<<<<<<<<<<<<dbuser: #{@dbuser.email_addr}.>>>>>>>>>>>>>"
+puts "[setup] goodattr: #{@good_attr}"
+  end
+
+  def badbadbad_setup
+    @bad_attr = {:email_addr => '', :password => ''}
+    @good_attr = {
+      :email_addr => 'iexist@test.org',
+      :password => 'existence-is-futile'
+    }
+    @user = Factory(:user)
+    dbuser = User.find_by_email_addr(@user.email_addr)
+    if dbuser == nil
+      # @user is not yet in the database.
+      if @user.save
+        dbuser = @user
+      else
+        throw "Retrieval of user at #{@user.email_addr} failed."
+      end
+else
+    end
+    @nogood_attr = {
+      :email_addr => dbuser.email_addr,
+      :password => dbuser.password
+    }
   end
 
   def test_bad_values
@@ -27,4 +68,27 @@ class SessionsControllerTest < ActionController::TestCase
       "flash error msg expected"
   end
 
+  describe "successful login" do
+
+    def test_good_values
+      post :create, :session => @good_attr
+      assert_response :success, 'normal response expected'
+#p "SCT self:", self
+#p 'SCT controller:', @controller
+p 'SCT controller class:', @controller.class
+p 'SCT controller current_user:', @controller.current_user
+#      assert controller.current_user == @user, 'expected current user'
+      #    assert_select 'title', /sign in/i, 'expected title'
+    end
+
+    def test_good_redirect
+puts "[test_good_redirect] goodattr: #{@good_attr}"
+      post :create, :session => @good_attr
+p 'SCT controller class:', @controller.class
+p 'SCT controller current_user:', @controller.current_user
+      assert_redirected_to user_path(@dbuser), "successful login redirect"
+      #    assert_select 'title', /sign in/i, 'expected title'
+    end
+
+  end
 end
