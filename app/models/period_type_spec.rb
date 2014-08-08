67967622c4a57387c6dfd6c9d7a44ab378431233
Jim Cochrane <jim.cochrane@gmail.com>
2014-08-08 00:39:51 -0500

    Adjust period-type-spec dates with respect to today
    The previous behavior used the start_date and end_date attributes of
    PeriodTypeSpec as is, which results in the situation where the start_date
    (if not edited) gets further and further in the past as time progresses,
    resulting in longer and longer time periods for requesting chart data and
    analysis.  Assuming that most users would prefer the start-date to be,
    essentially, now - n-days-ago (and the end-date usually nil - i.e., now -
    so that the covered time period stays the same length), I changed the
    behavior to use an "effective_start_date" and "effective_end_date" (for
    cases where the end date is specified/not-nil) such that these dates roll
    forward (based on today [now]) as the clock moves forward.  Changed the
    appropriate views (show and per-type-spec/edit) to use this scheme by using
    effective_start_date and effective_end_date (new queries of the
    PeriodTypeSpec class) instead of start_date and end_date.

--- app/models/period_type_spec.rb
+++ app/models/period_type_spec.rb
@@ -3,14 +3,14 @@
 # Table name: period_type_specs
 #
 #  id             :integer          not null, primary key
-#  period_type_id :integer
+#  period_type_id :integer          not null
 #  start_date     :datetime
 #  end_date       :datetime
 #  user_id        :integer
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
+#  category       :string(255)
 #
-
 
 class PeriodTypeSpec < ActiveRecord::Base
   include ActiveModel::Validations
@@ -27,11 +27,37 @@ class PeriodTypeSpec < ActiveRecord::Base
   SHORT_TERM = 'short-term'
   VALID_CATEGORY = { LONG_TERM => true, SHORT_TERM => true }
 
-  public
+  public ###  Access
 
+  # The start-date updated with respect to the current date
+  pre :start_date_exists do ! start_date.nil? end
+  post :result_exists do |result| result != nil end
+  def effective_start_date
+    if @eff_start_date.nil?
+      @eff_start_date = adjusted_datetime(start_date)
+    end
+    @eff_start_date
+  end
+
+  # The end-date updated with respect to the current date - nil iff
+  # end_date == nil
+  def effective_end_date
+    if end_date.nil?
+      if ! @eff_end_date.nil?
+        @eff_end_date = nil
+      end
+    elsif @eff_end_date.nil?
+      @eff_end_date = adjusted_datetime(end_date)
+    end
+    @eff_end_date
+  end
+
+  # The name corresponding to period_type_id
   def period_type_name
     PeriodTypeConstants.name_for[period_type_id]
   end
+
+  public ###  Status report
 
   # Is 'self' to be used for analysis?
   def for_analysis?
@@ -40,6 +66,25 @@ class PeriodTypeSpec < ActiveRecord::Base
 
   def invariant
     implies(category != nil, VALID_CATEGORY[category])
+  end
+
+  private
+
+  def today_at_midnight
+    now = DateTime.now
+    DateTime.new(now.year, now.month, now.day, 0)
+  end
+
+  def adjusted_datetime(dt)
+    today = today_at_midnight
+    correction_interval = updated_at.to_datetime.to_date -
+      dt.to_datetime.to_date
+    result = today - correction_interval
+    result = result.change({
+      :hour => dt.hour,
+      :min => dt.min,
+      :sec => dt.sec
+    })
   end
 
 end
